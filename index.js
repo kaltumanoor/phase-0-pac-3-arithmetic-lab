b83006e0484bd372c92a2388d8fe44332027a88f
kenhunja <garadcali@gmail.com>
2022-11-26 07:20:09 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(){}
+function subtract(){}
+function multiply(){}
+function divide(){}
+
+function add(a, b){
+	return a + b
+}
+
+function subtract(a, b){
+	return a - b
+}
+
+function multiply(a, b){
+	return a * b
+}
+
+function divide(a, b){
+	return a / b
+}
+
+function increment(n) {
+	return ++n
+}
+
+function decrement(n){
+	return --n
+}
+
+function makeInt(n){
+    
+	return parseInt('3',10);
+}
+
+function  makeInt(n){
+	return parseInt('0*2328',10)
+}
+
+function makeInt(n){
+    return parseInt('sldkjflksjf',10)
+}
